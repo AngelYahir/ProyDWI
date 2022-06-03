ec6b95b9549d96ebc6fbea81b7aa8845dedf40f5
Angel Torres <angel_torres2702@outlook.com>
2022-06-03 16:37:26 -0500

    React routes created

---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function NotFound() {
+  return (
+    <div class="mockup-window border bg-base-300">
+        <div class="flex justify-center px-4 py-16 bg-base-200">404 Not Found</div>
+    </div>
+  )
+}
