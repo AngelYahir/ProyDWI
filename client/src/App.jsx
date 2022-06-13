cc0328d176442b79cb29e47ff7c02bf92874fc4e
Zulma Ruiz <zulmadeyaniraruiz@gmail.com>
2022-06-13 03:00:42 -0500

    Orden de tarjetas, más página de categorias comenzada

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ import { Routes, Route } from 'react-router-dom'
 // * Import pages here
 import { 
   Home, 
-  NotFound 
+  NotFound,
+  Categories 
 } from './pages'
 
 // * Import components here
@@ -19,6 +20,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="*" element={<NotFound/>} />
+        <Route path="/" element={<Categories/>} />
       </Routes>
     </>
   )
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,2 +1,3 @@
 export {Home} from './Home'
 export {NotFound} from './NotFound'
+export {Categories} from './Categories'
