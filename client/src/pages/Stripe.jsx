6dab6d326e479415def273e853544a37154632ee
Zulma Ruiz <zulmadeyaniraruiz@gmail.com>
2022-08-07 12:18:17 -0500

    stripe

--- client/src/pages/Stripe.jsx
+++ client/src/pages/Stripe.jsx
@@ -4,7 +4,6 @@ import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-import "react-toastify/dist/ReactToastify.css";
 import "./styles.css";
 
 toast.configure();
